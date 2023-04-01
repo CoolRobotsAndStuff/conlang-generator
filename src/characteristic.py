7c647fd6ab082d8863b1b8b174392a82184e7c8c
ale <aledeum.saf@gmail.com>
2023-04-01 06:32:27 -0300

    fixed function to set_characteristic and added function to get string representation to CharacteristicStack class

--- src/characteristic.py
+++ src/characteristic.py
@@ -35,9 +35,38 @@ class CharacteristicStack(Stack, Characteristic):
     def apply(self, base):
         for key, characteristic in self.members.items():
             base = characteristic.apply(base)
-            print("Applied", key)
         return base
 
     def set_characteristic(self, key: str, value: str):
-        self.members[key].set_value(value)
+        for member_key, member_value in self.members.items():
+            if isinstance(member_value, ComplexCharacteristic):
+                if key == member_key:
+                    member_value.set_value(value)
+
+            elif isinstance(member_value, CharacteristicStack):
+                member_value.set_characteristic(key, value)
+
+    def get_string(self, indentation=0, do_number=False):
+        final_str = ""
+        indent = indentation * "  "
+        number = 0
+        for member_key, member_value in self.members.items():
+            if isinstance(member_value, CharacteristicStack):
+                final_str +=  f"{number if do_number else ''}- {member_key}:\n"
+                final_str += member_value.get_string(indentation + 1)
+
+            elif isinstance(member_value, ComplexCharacteristic):
+                final_str +=  f"{indent}{number if do_number else ''}. {member_key}: {member_value.value} \n"
+            else:
+                final_str +=  f"{indent}{number if do_number else ''}. {member_key}\n"
+
+            number += 1
+        
+        return final_str
+
+
+    def __str__(self):
+        heading = "---Characteristics---\n"
+
+        return  heading + self.get_string(0, True)
     
